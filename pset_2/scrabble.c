58e6aa02e8daca4f48be419e2ff01a3bc6cc5b94
Alridho <alridho2003@gmail.com>
2023-02-15 14:31:48 +0800

    scrabble done

--- pset_2/scrabble.c
+++ pset_2/scrabble.c
@@ -5,7 +5,7 @@
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int compute_score(string word);
+int compute_score(char word[]);
 
 int main(void)
 {
@@ -17,25 +17,40 @@ int main(void)
     scanf("%s", &player2);
 
     // Score both words
-    int score1 = compute_score(word1);
-    int score2 = compute_score(word2);
+    int score1 = compute_score(player1);
+    int score2 = compute_score(player2);
 
     // TODO: Print the winner
     if (score1 > score2)
     {
-        printf("Player 1 wins!");
+        printf("Player 1 wins!\n");
     }
     else if (score1 < score2)
     {
-        printf("Player 2 wins!")
+        printf("Player 2 wins!\n");
     }
     else
     {
-        printf("Tie!");
+        printf("Tie!\n");
     }
+    printf("%i %i\n", score1, score2);
 }
 
-int compute_score(string word)
+int compute_score(char word[])
 {
     // TODO: Compute and return score for string
+    int score = 0;
+
+    for (int i = 0; i < strlen(word); i++)
+    {
+        if (isupper(word[i]))
+        {
+            score += POINTS[word[i] - 65]; //ascii
+        }
+        else if (islower(word[i]))
+        {
+            score += POINTS[word[i] - 97];
+        }
+    }
+    return score;
 }
